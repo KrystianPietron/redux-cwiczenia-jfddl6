5f4729b441f370093de854a9797117098e4484f4
Krystian Pietroń <krystianpietr@gmail.com>
2018-12-03 10:54:49 +0100

    redux

--- src/Store.js
+++ src/Store.js
@@ -1,3 +1,16 @@
-export default (state = [], action) =>{
-    return state
+const ADD_TODO = 'ADD_TODO'
+
+export const addToDo = text => ({
+    type: ADD_TODO,
+    text
+})
+
+export default (state = [], action) => {
+    switch (action.type) {
+        case ADD_TODO:
+            const newToDo = { text: action.text, completed: false }
+            return [...state, newToDo]
+        default:
+            return state
+    }
 }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,19 @@ import './index.css';
 import App from './App';
 
 import { combineReducers, createStore } from 'redux'
+import todos, { addToDo } from './Store'
 
 
-
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+    todos
+})
 const store = createStore(rootReducer)
+
+console.warn(store.getState())
+store.dispatch(addToDo('Go Shopping'))
+store.dispatch(addToDo('Some other thing'))
+store.dispatch(addToDo('Clean the house'))
+console.warn(store.getState())
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
