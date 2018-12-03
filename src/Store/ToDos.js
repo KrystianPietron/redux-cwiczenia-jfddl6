7999e5bced7cb7160a41bab5d3f221ba302fbee3
Krystian Pietroń <krystianpietr@gmail.com>
2018-12-03 14:39:58 +0100

    add delete method for todo

--- src/Store/ToDos.js
+++ src/Store/ToDos.js
@@ -1,18 +1,29 @@
 const ADD_TODO = 'ADD_TODO'
 const FILTER_TODO = 'FILTER_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const DELETE_TODO = 'DELETE_TODO'
 
 const INITIAL_STATE = {
     allTodos: [],
-    filterTodos: []
+    visibleTodos: []
 }
 
 export const addToDo = text => ({
     type: ADD_TODO,
     text
 })
-export const filterTodos = text => ({
+export const visibleTodos = text => ({
     type: FILTER_TODO,
     input: text
+})
+export const toggleTodo = index => ({
+    type: TOGGLE_TODO,
+    index
+})
+
+export const deleteTodo = index => ({
+    type: DELETE_TODO,
+    index
 })
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
@@ -28,7 +39,25 @@ export default (state = INITIAL_STATE, action) => {
         case FILTER_TODO:
             return {
                 ...state,
-                filterTodos: state.allTodos.filter(todo => todo.text.includes(action.input))
+                visibleTodos: state.allTodos.filter(todo => todo.text.includes(action.input))
+            }
+        case TOGGLE_TODO:
+            return {
+                ...state,
+                allTodos: state.allTodos.map((todo, index) => {
+                    if (index === action.index) {
+                        return {
+                            ...todo,
+                            completed: !todo.completed
+                        }
+                    }
+                    return todo
+                })
+            }
+            case DELETE_TODO:
+            return{
+                ...state,
+                allTodos: state.allTodos.filter((todo, index) => (index !== action.index))
             }
         default:
             return state
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 import { combineReducers, createStore } from 'redux'
-import todos, { addToDo, filterTodos } from './Store/ToDos'
+import todos, { addToDo, visibleTodos, toggleTodo, deleteTodo } from './Store/ToDos'
 import Counter, { addInc, addDec, reset } from './Store/Counter'
 import ItemsList, { addItem } from './Store/ItemsList'
 
@@ -19,9 +19,9 @@ const store = createStore(rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-// store.dispatch(addToDo('Go Shopping'))
-// store.dispatch(addToDo('Some other thing'))
-// store.dispatch(addToDo('Clean the house'))
+store.dispatch(addToDo('Go Shopping'))
+store.dispatch(addToDo('Some other thing'))
+store.dispatch(addToDo('Clean the house'))
 // store.dispatch(addInc())
 // store.dispatch(addInc())
 // store.dispatch(addInc())
@@ -31,8 +31,9 @@ window.addToDo = (text) => store.dispatch(addToDo(text))
 window.decrement = () => store.dispatch(addDec())
 window.restart = () => store.dispatch(reset())
 window.addItem = (title, price) => store.dispatch(addItem(title, price))
-window.filter = (text) => store.dispatch(filterTodos(text))
-
+window.filter = (text) => store.dispatch(visibleTodos(text))
+window.toggle = (index) => store.dispatch(toggleTodo(index))
+window.delete = (index) => store.dispatch(deleteTodo(index))
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
